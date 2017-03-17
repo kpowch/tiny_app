c66bb50756d5394f0a30102b347f7445d4402d15
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-17 03:55:39 +0000

    modifies app so links belong to users and can only be modified by users. also stores passwords securly

--- express_server.js
+++ express_server.js
@@ -12,10 +12,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+// for password hashing
+const bcrypt = require('bcrypt');
+
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  'b2xVn2': {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: 'userRandomID'
+  },
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: 'user2RandomID'
+  }
 };
 
 const usersDatabase = {
@@ -23,17 +32,15 @@ const usersDatabase = {
     id: 'userRandomID',
     name: 'John Smith',
     email: 'user@example.com',
-    password: 'purple-monkey-dinosaur'
+    password: bcrypt.hashSync('purple-monkey-dinosaur', 10)
   },
   'user2RandomID': {
     id: 'user2RandomID',
     name: 'Janice Smart',
     email: 'user2@example.com',
-    password: 'dishwasher-funk'
+    password: bcrypt.hashSync('dishwasher-funk', 10)
   }
 };
-
-
 
 
 
@@ -54,11 +61,22 @@ function propInUserDatabase(propKey, propValue) {
 
 function passwordMatch(email, password) {
   for (let key in usersDatabase) {
-    if (usersDatabase[key].email === email && usersDatabase[key].password === password) {
+    if (usersDatabase[key].email === email
+        && bcrypt.compareSync(password, usersDatabase[key].password)) {
       return key;
     }
   }
   return false;
+}
+
+function urlsForUser(id) {
+  let urlsForUser = {};
+  for (let prop in urlDatabase) {
+    if (urlDatabase[prop].userID === id) {
+      urlsForUser[prop] = urlDatabase[prop]
+    }
+  }
+  return urlsForUser;
 }
 
 
@@ -94,9 +112,8 @@ app.post('/register', (req, res) => {
       id: user_id,
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: bcrypt.hashSync(req.body.password, 10)
     };
-    // console.log(usersDatabase);
     res.cookie('user_id', user_id);
     res.redirect('/urls');
   } else {
@@ -136,6 +153,7 @@ app.post('/logout', (req, res) => {
 });
 
 // Create short URL for inputted long URL
+//TODO the link has to start with http:// - should make an error? or concatenate?
 app.get('/urls/new', (req, res) => {
   if(req.cookies.user_id) {
     let templateVars = {
@@ -145,29 +163,28 @@ app.get('/urls/new', (req, res) => {
   } else {
     res.redirect('/login');
   }
-
-
-
 });
 
 app.post('/urls', (req, res) => {
   const newShortURL = generateRandomString(6);
-  urlDatabase[newShortURL] = req.body.longURL;
+  urlDatabase[newShortURL] = {
+    longURL: req.body.longURL,
+    userID: req.cookies.user_id
+  };
   res.redirect(`/urls/${newShortURL}`);
 });
 
 // Retrieve index page of all URLs
 app.get('/urls', (req, res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    user: undefined
-  };
   if (req.cookies.user_id) {
-    templateVars.user = usersDatabase[req.cookies.user_id];
+    const templateVars = {
+      urls: urlsForUser(req.cookies.user_id),
+      user: usersDatabase[req.cookies.user_id]
+    };
+    res.render('urls_index', templateVars);
+  } else {
+    res.send('not logged in')
   }
-
-  res.render('urls_index', templateVars);
-  //}
 });
 
 // Update specified URL
@@ -175,53 +192,47 @@ app.post('/urls/:id', (req, res) => {
   if(!req.body.longURL) {
     res.end('need to put new URL');
   } else {
-    urlDatabase[req.params.id] = req.body.longURL;
+    urlDatabase[req.params.id]['longURL'] = req.body.longURL;
     res.redirect('/urls');
   }
 });
 
 // Delete specified URL
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
+  if(urlDatabase[req.params.id].userID === req.cookies.user_id) {
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+  } else {
+    res.send('you can only delete your own links')
+  }
 });
 
 // Retrieve specified URL
 // note: this must be after the /urls/new and /urls/:id/delete otherwise they don't work
 app.get('/urls/:id', (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    urls: urlDatabase,
-    user: undefined
-  };
-  if (req.cookies.user_id) {
-    templateVars.user = usersDatabase[req.cookies.user_id];
+  if (!urlDatabase[req.params.id]) {
+    res.status(404).send("<img src='https://http.cat/404' alt='404! Page not found.' style='width:100%;'>");
   }
 
-  if(!urlDatabase[req.params.id]) {
-    res.status(404).send("<img src='https://http.cat/404' alt='404! Page not found.' style='width:100%;'>");
-  } else {
+  if (urlDatabase[req.params.id].userID === req.cookies.user_id) {
+    const templateVars = {
+      shortURL: req.params.id,
+      urls: urlDatabase,
+      user: usersDatabase[req.cookies.user_id]
+    };
     res.render('urls_show', templateVars);
+  } else {
+    res.send('you can only edit your own links')
   }
 });
 
 // to redirect short URLs to their longURL page
 app.get('/u/:shortURL', (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    urls: urlDatabase,
-    user: undefined
-  };
-  if (req.cookies.user_id) {
-    templateVars.user = usersDatabase[req.cookies.user_id];
-  }
-
   if(!urlDatabase[req.params.shortURL]) {
     res.status(404).send("<img src='https://http.cat/404' alt='404! Page not found.' style='width:100%;'>");
     // res.sendStatus(404) // equivalent to res.status(404).send('Not Found')
   } else {
-    let longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
   }
 });
 
